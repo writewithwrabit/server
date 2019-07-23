f973e92ca2dc1a1ce0bde505538b2d2eb9ad1260
Anthony Morris <anthony@amorrissound.com>
2019-07-22 20:20:54 -0700

    Add API authorization using Firebase

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-  "os"
+	"os"
 
+	firebase "firebase.google.com/go"
 	"github.com/99designs/gqlgen/handler"
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/writewithwrabit/server/auth"
 	graphql "github.com/writewithwrabit/server/graphql"
+	"google.golang.org/api/option"
 )
 
 const defaultPort = "8080"
@@ -23,18 +28,33 @@ func main() {
 		log.Println("File .env not found!")
 	}
 
+	router := chi.NewRouter()
+
 	db = DB()
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
-  }
+	}
 
-	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(graphql.New(db))))
+	opt := option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	router.Use(auth.Middleware(client))
+
+	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	router.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(graphql.New(db))))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
 // DB gets a connection to the database.
@@ -57,8 +77,8 @@ func DB() *sql.DB {
 	dbURI := fmt.Sprintf("host=/cloudsql/%s dbname=%s user=%s password=%s", connectionName, dbName, user, password)
 	dialer := "postgres"
 	if env == "dev" {
-    dbURI = fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable", connectionName, dbName, user, password)
-    dialer = "cloudsqlpostgres"
+		dbURI = fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable", connectionName, dbName, user, password)
+		dialer = "cloudsqlpostgres"
 	}
 
 	conn, err := sql.Open(dialer, dbURI)
