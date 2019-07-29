54824cbd0f8752cad915d04190c7cc55e6863fd7
Anthony Morris <anthony@amorrissound.com>
2019-07-28 20:40:04 -0700

    Add created_at and updated_at columns

--- db/wrabit.sql
+++ db/wrabit.sql
@@ -4,7 +4,9 @@ CREATE TABLE users (
   first_name VARCHAR,
   last_name VARCHAR,
   email VARCHAR,
-  word_goal INT NOT NULL DEFAULT 1000
+  word_goal INT NOT NULL DEFAULT 1000,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE editors (
@@ -12,20 +14,54 @@ CREATE TABLE editors (
   user_id VARCHAR,
   show_toolbar BOOLEAN NOT NULL DEFAULT true,
   show_prompt BOOLEAN NOT NULL DEFAULT false,
-  show_counter BOOLEAN NOT NULL DEFAULT true
+  show_counter BOOLEAN NOT NULL DEFAULT true,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE entries (
   id SERIAL,
   user_id VARCHAR,
   word_count INT,
-  content TEXT
+  content TEXT,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE streaks (
   id SERIAL,
   user_id VARCHAR,
-  day_count INT
+  day_count INT,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE OR REPLACE FUNCTION trigger_updated()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updated
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_updated();
+
+CREATE TRIGGER updated
+BEFORE UPDATE ON editors
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_updated();
+
+CREATE TRIGGER updated
+BEFORE UPDATE ON entries
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_updated();
+
+CREATE TRIGGER updated
+BEFORE UPDATE ON streaks
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_updated();
 
 INSERT INTO users (firebase_id, first_name, last_name, email, word_goal) VALUES ('0T3AWCd9mkdDFPeV0SDXqj3GRvZ2', 'Anthony', 'Morris', 'anthony@amorrissound.com', 1000);
