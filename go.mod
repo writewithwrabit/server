d5a17602ab9f07a5e1e0377e7f1a149c48445178
Anthony Morris <anthony@amorrissound.com>
2019-09-15 17:22:35 -0700

    Filter out entries with no content (#9)

--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
 module github.com/writewithwrabit/server
 
+go 1.12
+
 require (
-	firebase.google.com/go v3.8.1+incompatible
-	github.com/99designs/gqlgen v0.9.1
-	github.com/GoogleCloudPlatform/cloudsql-proxy v0.0.0-20190711182328-f0b00cc64402
+	firebase.google.com/go v3.9.0+incompatible
+	github.com/99designs/gqlgen v0.9.3
+	github.com/GoogleCloudPlatform/cloudsql-proxy v0.0.0-20190828224159-d93c53a4824c
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/cors v1.0.0
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.2.0
 	github.com/vektah/gqlparser v1.1.2
-	google.golang.org/api v0.7.0
+	google.golang.org/api v0.10.0
 )
--- main.go
+++ main.go
@@ -31,19 +31,19 @@ func main() {
 
 	env := os.Getenv("NODE_ENV")
 
-  router := chi.NewRouter()
-  
-  // Basic CORS
-  // for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
-  cors := cors.New(cors.Options{
-    AllowedOrigins:   []string{"*"},
-    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-    ExposedHeaders:   []string{"Link"},
-    AllowCredentials: true,
-    MaxAge:           300, // Maximum value not ignored by any of major browsers
-  })
-  router.Use(cors.Handler)
+	router := chi.NewRouter()
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	router.Use(cors.Handler)
 
 	db = DB()
 
