2cd6484e9eaae6835f8e9c315d1be0b2b886be72
Anthony Morris <anthony@amorrissound.com>
2019-12-03 18:36:47 -0800

    Fix some issues with the dynamic goal logic

--- graphql/resolver.go
+++ graphql/resolver.go
@@ -538,13 +538,14 @@ func (r *queryResolver) DailyEntry(ctx context.Context, userID string, date stri
 }
 
 func (r *queryResolver) WordGoal(ctx context.Context, userID string) (int, error) {
-	if user := auth.ForContext(ctx); user == nil {
+	ctxUser := auth.ForContext(ctx)
+	if ctxUser == nil || ctxUser.Subject != userID {
 		return 0, fmt.Errorf("Access denied")
 	}
 
-	res := wrabitDB.LogAndQueryRow(r.db, "SELECT firebase_id, word_goal FROM users WHERE id = $1", userID)
+	res := wrabitDB.LogAndQueryRow(r.db, "SELECT word_goal FROM users WHERE firebase_id = $1", userID)
 	var user User
-	if err := res.Scan(&user.FirebaseID, &user.WordGoal); err != nil {
+	if err := res.Scan(&user.WordGoal); err != nil {
 		panic(err)
 	}
 
@@ -554,14 +555,14 @@ func (r *queryResolver) WordGoal(ctx context.Context, userID string) (int, error
 	daySinceLastWrote := 0
 
 	// Attempt to get multiplier from a running streak
-	res = wrabitDB.LogAndQueryRow(r.db, "SELECT day_count FROM streaks WHERE user_id = $1 AND date_part('day', updated_at) <= date_part('day', NOW() - '1 day'::INTERVAL);", user.FirebaseID)
+	res = wrabitDB.LogAndQueryRow(r.db, "SELECT day_count FROM streaks WHERE user_id = $1 AND date_part('day', updated_at) <= date_part('day', NOW() - '1 day'::INTERVAL);", userID)
 	err := res.Scan(&streakDayCount)
 	if err != nil && err != sql.ErrNoRows {
 		panic(err)
 	}
 
 	// Figure out when the last entry was written
-	res = wrabitDB.LogAndQueryRow(r.db, "SELECT date_part('day', NOW() - updated_at::timestamp) As day_since_last_entry FROM entries WHERE user_id = $1;", user.FirebaseID)
+	res = wrabitDB.LogAndQueryRow(r.db, "SELECT date_part('day', NOW() - updated_at::timestamp) As day_since_last_entry FROM entries WHERE user_id = $1 AND goal_hit = true ORDER BY updated_at;", userID)
 	err = res.Scan(&daySinceLastWrote)
 	if err != nil && err != sql.ErrNoRows {
 		panic(err)
@@ -575,7 +576,7 @@ func (r *queryResolver) WordGoal(ctx context.Context, userID string) (int, error
 		// 3 --> 0.7
 		// ...
 		multiplier = multiplier - (float64(daySinceLastWrote) * 0.1)
-	} else if daySinceLastWrote > 10 {
+	} else if daySinceLastWrote > 9 || err != nil && err == sql.ErrNoRows {
 		multiplier = 0.1
 	}
 
