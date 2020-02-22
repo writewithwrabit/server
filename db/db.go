ae3685575b8130a8dc5d04c4a974a16114c49695
Anthony Morris <anthony@amorrissound.com>
2020-02-22 12:39:32 -0800

    Log args to queries for easier debugging/time travel

--- db/db.go
+++ db/db.go
@@ -7,6 +7,7 @@ import (
 
 func LogAndQuery(db *sql.DB, query string, args ...interface{}) *sql.Rows {
 	fmt.Println(query)
+	fmt.Println(args...)
 
 	res, err := db.Query(query, args...)
 	if err != nil {
@@ -18,12 +19,14 @@ func LogAndQuery(db *sql.DB, query string, args ...interface{}) *sql.Rows {
 
 func LogAndQueryRow(db *sql.DB, query string, args ...interface{}) *sql.Row {
 	fmt.Println(query)
+	fmt.Println(args...)
 
 	return db.QueryRow(query, args...)
 }
 
 func LogAndExec(db *sql.DB, query string, args ...interface{}) sql.Result {
 	fmt.Println(query)
+	fmt.Println(args...)
 
 	res, err := db.Exec(query, args...)
 	if err != nil {
